ba5dc2d1611da6bfe2d9c812b469efbc719fd023
gopaybis <vlessgithub1@outlook.co.id>
2025-06-23 00:30:22 +0700

    Create checkProxy.js

---
+++ api/checkProxy.js
@@ -0,0 +1,15 @@
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    try {
+      const { ip, port } = req.body;
+      const url = `https://apihealtcheck.vercel.app/api/v1?ip=${ip}&port=${port}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      res.status(200).json(data);
+    } catch (error) {
+      res.status(500).json({ error: true });
+    }
+  } else {
+    res.status(405).json({ error: "Method Not Allowed" });
+  }
+}
